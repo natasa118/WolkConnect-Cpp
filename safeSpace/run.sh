4b41e06706df500e245660bc95b08ec1d262cae4
Natasa <balognatasa118@gmail.com>
2025-07-09 15:54:04 +0200

    Added safespace with dockerfile

---
+++ safeSpace/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker container stop debuilder
+docker container rm debuilder
+
+docker build -t mytask .
+
+docker run -dit --name debuilder --cpus $(nproc) mytask || exit
+docker exec -it debuilder unzip /build/*.zip -d WolkConnect-Cpp  || exit
+docker exec -it debuilder /build/make_deb.sh $branch 
+docker cp debuilder:/build/ .
+
+rm ./build/*.zip
+
+docker container stop debuilder
+docker container rm debuilder
